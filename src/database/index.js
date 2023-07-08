282b8a9b2d2e0cf0e4bb7d72ab13b74c0c629966
ngohongthong <baphongpine@gmail.com>
2023-07-08 21:54:11 +0700

    docker mySQL ( so bad )

---
+++ src/database/index.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    // host: 'localhost', // replace with your MySQL host
+    // user: 'root', // replace with your MySQL username
+    // password: '', // replace with your MySQL password
+    // database: 'mini-game-change-color', // replace with your MySQL database name
+
+    // The following are not needed for local development
+    host: 'localhost',
+    user: 'root',
+    password: 'mysecretpassword',
+    database: 'mydatabase',
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+  } else {
+    console.log('Connected to MySQL database');
+  }
+});
+
+module.exports = connection;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const cors  = require("cors")
+const connection = require("./database/index")
+const bodyParser = require("body-parser")
 
 
 const app = express()
 
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 const res = (req, res) => {
     res.json({
@@ -30,6 +34,76 @@ app.get("/learn", (req, res) => {
         status: "Done"
     })
 })
+app.get("/users", (req, res) => {
+
+    const query = 'SELECT * FROM `user-info`';
+    connection.query(query, (err, result) => {
+        if(err) {
+            res.json({
+                message: "Error"
+            })
+        } else {
+            res.json({
+                message: "Success",
+                data: result
+            })
+        }
+    })
+})
+app.post("/add-user", (req, res) => {
+    const {name, age, skill} = req.body
+
+    const query = `INSERT INTO \`user-info\` (\`name\`, \`age\`, \`skill\`) VALUES ('${name}', '${age}', '${skill}')`;
+    connection.query(query, (err, result) => {
+        if(err) {
+            res.json({
+                message: "Error"
+            })
+        } else {
+            res.json({
+                message: "Success",
+                data: result
+            })
+        }
+    })
+})
+app.post("/delete-user/:id", (req, res) => {
+    const {id} = req.params
+    const query = `DELETE FROM \`user-info\` WHERE id = ${id}`;
+    connection.query(query, (err, result) => {
+        if(err) {
+            res.json({
+                message : "Error"
+            })
+        } else {
+            res.json({
+                message: "Success",
+                data: result
+            })
+        }
+    })
+})
+app.post("/update-user/:id", (req, res) => {
+    const {id} = req.params
+    const {name, age, skill} = req.body
+    const query = `UPDATE \`user-info\` SET \`name\`='${name}',\`age\`='${age}',\`skill\`='${skill}' WHERE id = ${id}`;
+    connection.query(query, (err, result) => {
+        if(err) {
+            res.json({
+                message : "Error"
+            })
+        } else {
+            res.json({
+                message: "Success",
+                data: result
+            })
+        }
+    })
+})
+    
+
+
+
 
 
 app.listen(3000, () => {
